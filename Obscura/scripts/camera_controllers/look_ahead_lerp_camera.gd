2efd0b18dbd0f86bad6d7bc7b79716fb50657591
Jack Schonherr <jack.schonherr@gmail.com>
2024-10-30 09:23:16 -0700

    way better lerping

--- Obscura/scripts/camera_controllers/look_ahead_lerp_camera.gd
+++ Obscura/scripts/camera_controllers/look_ahead_lerp_camera.gd
@@ -3,7 +3,7 @@ extends CameraControllerBase
 
 const STUTTER_BUFFER = 0.01
 
-@export var lead_speed:float = target.BASE_SPEED * 1.5
+@export var lead_speed:float = 5
 @export var catchup_delay_duration:float  = 0.5
 @export var catchup_speed:float = 5
 @export var leash_distance:float = 5
@@ -19,32 +19,26 @@ func _process(delta: float) -> void:
 	
 	if draw_camera_logic:
 		draw_logic()
-	
-	var x_diff:float = target.global_position.x - global_position.x
-	var z_diff:float = target.global_position.z - global_position.z	#
+		
 	var cpos_vec2:Vector2 = Vector2(global_position.x, global_position.z)
 	var tpos_vec2:Vector2 = Vector2(target.global_position.x, target.global_position.z)
 	var dist:float = cpos_vec2.distance_to(tpos_vec2)
-	
+
 	if dist != 0:
-		if target.velocity != Vector3(0,0,0):
-			if dist < leash_distance:
-				print("<")
-				var look_ahead_pos = target.global_position + target.velocity.normalized() * leash_distance
-				cpos_vec2 = cpos_vec2.lerp(Vector2(look_ahead_pos.x, look_ahead_pos.z), 5 * delta)
-				global_position.x = cpos_vec2.x
-				global_position.z = cpos_vec2.y
-			else:
-				print(">")
-				var diff_vector = Vector3(x_diff, 0, z_diff)
-				global_position.x = target.global_position.x - diff_vector.normalized().x * leash_distance
-				global_position.z = target.global_position.z - diff_vector.normalized().z * leash_distance
+		if target.velocity != Vector3(0, 0, 0):
+			var look_ahead_pos = target.global_position + target.velocity.normalized() * leash_distance
+			cpos_vec2 = cpos_vec2.lerp(Vector2(look_ahead_pos.x, look_ahead_pos.z), lead_speed * delta)
+			global_position.x = target.velocity.x * delta + cpos_vec2.x
+			global_position.z = target.velocity.z * delta + cpos_vec2.y
 		else:
-			global_position = global_position.lerp(target.global_position, catchup_speed * delta)
+			cpos_vec2 = cpos_vec2.lerp(tpos_vec2, catchup_speed * delta)
+			global_position.x = cpos_vec2.x
+			global_position.z = cpos_vec2.y
 		
 	if dist < STUTTER_BUFFER and target.velocity == Vector3(0, 0, 0):
-		global_position = target.global_position
-		
+		global_position.x = target.global_position.x	
+		global_position.z = target.global_position.z	
+			
 	super(delta)
 	
 	
--- Obscura/scripts/camera_controllers/position_lock_lerp.gd
+++ Obscura/scripts/camera_controllers/position_lock_lerp.gd
@@ -3,9 +3,9 @@ extends CameraControllerBase
 
 const STUTTER_BUFFER = 0.01
 
-@export var follow_speed:float = 1
+@export var follow_speed:float = 5
 @export var catchup_speed:float = 5
-@export var leash_distance:float = 7
+@export var leash_distance:float = 5
 
 func _ready() -> void:
 	super()
@@ -19,28 +19,25 @@ func _process(delta: float) -> void:
 	if draw_camera_logic:
 		draw_logic()
 	
-	var x_diff:float = target.global_position.x - global_position.x
-	var z_diff:float = target.global_position.z - global_position.z	#
 	var cpos_vec2:Vector2 = Vector2(global_position.x, global_position.z)
 	var tpos_vec2:Vector2 = Vector2(target.global_position.x, target.global_position.z)
 	var dist:float = cpos_vec2.distance_to(tpos_vec2)
-	
+
 	if dist != 0:
-		if target.velocity != Vector3(0,0,0):
-			if dist < leash_distance:
-				cpos_vec2 = cpos_vec2.lerp(tpos_vec2, follow_speed * delta)
-				global_position.x = cpos_vec2.x
-				global_position.z = cpos_vec2.y
-			else:
-				var diff_vector = Vector3(x_diff, 0, z_diff)
-				global_position.x = target.global_position.x - diff_vector.normalized().x * leash_distance
-				global_position.z = target.global_position.z - diff_vector.normalized().z * leash_distance
+		if target.velocity != Vector3(0, 0, 0):
+			var new_pos = target.global_position + -1 * target.velocity.normalized() * leash_distance
+			cpos_vec2 = cpos_vec2.lerp(Vector2(new_pos.x, new_pos.z), follow_speed * delta)
+			global_position.x = target.velocity.x * delta + cpos_vec2.x
+			global_position.z = target.velocity.z * delta + cpos_vec2.y
 		else:
-			global_position = global_position.lerp(target.global_position, catchup_speed * delta)
+			cpos_vec2 = cpos_vec2.lerp(tpos_vec2, catchup_speed * delta)
+			global_position.x = cpos_vec2.x
+			global_position.z = cpos_vec2.y
 		
 	if dist < STUTTER_BUFFER and target.velocity == Vector3(0, 0, 0):
-		global_position = target.global_position
-	
+		global_position.x = target.global_position.x	
+		global_position.z = target.global_position.z	
+
 	super(delta)
 	
 func draw_logic() -> void:
